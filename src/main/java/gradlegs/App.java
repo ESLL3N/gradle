1ce990ec00b440be4e6e821bc092f0c7caa0250b
ESLL3N <esllenescolar@gmail.com>
2022-05-02 17:57:15 +0000

    herança

--- src/main/java/gradlegs/App.java
+++ src/main/java/gradlegs/App.java
@@ -1,12 +1,21 @@
 package gradlegs;
 
-public class App {
+public  class App {
     public static void main(String[] args) {
         Genero g = new Genero();
-        g.setId(-10);
+        g.setId(10);
         g.setNome("Aventura");
-        //System.out.println("olá mundo");
+        
+        PessoaFisica p = new PessoaFisica();
+        p.setNome("José da Silva");
+        p.setCpf("65498712354");
+
+        System.out.println("Olá Gradle");
         System.out.println(g.getNome());
     
     }
 }
+
+// o atributo protegido ele só é acessivel pelas classe herdeiras e a propria classe
+//atributo funciona como uma variavel da classe 
+//no metodo privado somente quem está na mesma classe pode acessá-lo 
